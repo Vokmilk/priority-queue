ab0b2362e8ce3ff692439e366be005c3f9323f13
Vokmilk <shadowcool12@gmail.com>
2019-03-24 00:21:36 +0300

    add: append,removeChild,remove

--- src/node.js
+++ src/node.js
@@ -1,22 +1,44 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data=data;
+		this.priority=priority;
+		this.parent=null;
+		this.right=null;
+		this.left=null;
 	}
 
 	appendChild(node) {
 
+		if (!this.left) {
+			this.left = node;
+			node.parent=this;}
+		else if (!this.right) {
+				this.right = node;
+				node.parent=this;
+			}
 	}
 
 	removeChild(node) {
-
+		if (this.left == node) {
+			this.left = null;
+			node.parent=null;} 
+		else if (this.right == node) {	
+			this.right = null;
+			}
+			else {
+				alert('node is not a child of this node')
+				
+			}
 	}
 
 	remove() {
-
+		if(this.parent)
+		this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+		if (this.parent) {
+		}
 	}
 }
 
